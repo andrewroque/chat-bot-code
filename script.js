9062254d6a4a129ad23c0e0c4e89555989849d8c
andrewroque <78003117+andrewroque@users.noreply.github.com>
2023-02-23 14:53:30 -0800

    added voicemails

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const recognition = new SpeechRecognition();
 
 const voicemails = [];
 voicemails.push("Hi babe. How are you? I just missed you. Hope you could call me tonight. Thank you. I love you, babe.");
-voicemails.push("Hi babe. Just wanna say how you two guys are doing. Okay. Love.");
+voicemails.push("Hi babe. Just wanna say how you two guys are doing. Okay. Love you");
 voicemails.push("Babe, I meant to, um, greet you. Happy anniversary. I forgot all about it. I've been wanting to call you. Okay. Hope you had a really good anniversary. It's been like three years. Wow. Okay. See you. I'm happy you could stay a little longer with me this time. All right. Bye.");
 voicemails.push("Hi, babe. Okay. Thank you.");
 voicemails.push("Babe, please call me again.");
@@ -15,10 +15,13 @@ voicemails.push("I just go straight five, right? So I don't go to one 10 and all
 voicemails.push("Hey, where are you? Are you coming for lunch? I'm preparing for you to come to lunch. Let me know.");
 voicemails.push("Hello? Katie, you called me. I, sorry, I messed up. I just got in in my room. Okay, bye.");
 voicemails.push("How are you? Happy Thanksgiving. I don't know what you're doing. Bye. Call me when you can.");
+voicemails.push("Hi, babe. Sorry I missed your call. Bye");
+voicemails.push("Babe. Happy birthday. I know you're busy. Okay, take care. I love you. Bye.");
+voicemails.push("Hi, babe. Call me when you can.");
 
 
 const random = voicemails[Math.floor(Math.random() * voicemails.length)];
-const randomOutput = voicemails[random];
+// const randomOutput = voicemails[random];
 
 // console.log(random);
 
@@ -62,4 +65,5 @@ function determineWords(speech,words) {
 
 button.addEventListener("click", () =>{
   recognition.start();
+  const random = voicemails[Math.floor(Math.random() * voicemails.length)];
 });
